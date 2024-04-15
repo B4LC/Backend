5e14fbef2c8f161fe7dfefd4981acddffebf6adb
NeedAvailableName <monkeydangluffy@gmail.com>
2024-04-15 14:19:03 +0700

    feat: migrate to fantom testnet

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
-const CONTRACT_ADDRESS = "0xf22657Af36847977299719f54C6e8e71B4fB0a2b"
-const THEGRAPH_URI = "https://api.studio.thegraph.com/query/34554/b4lc-testv1/v4.1.1"
+const CONTRACT_ADDRESS = "0x2DCA534a86fb267Fa109E1849694F1c36E118314"
+const THEGRAPH_URI = "https://api.studio.thegraph.com/query/34554/b4lc-testv1/v4.1.2"
 export {
     CONTRACT_ADDRESS,
     THEGRAPH_URI
